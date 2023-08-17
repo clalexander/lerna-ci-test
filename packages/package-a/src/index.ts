79f8d276ab0a975312501585a2c8fecef5f6e305
Chris Alexander <chris@synaptechgenesis.com>
2023-08-17 12:56:57 -0400

    feat: add say hola

--- packages/package-a/src/index.ts
+++ packages/package-a/src/index.ts
@@ -2,3 +2,5 @@
 export const sayHello = (name: string) => `Hello ${name}!`;
 // new feature comment
 export const sayGoodbye = (name: string) => `Goodbye ${name}!`;
+// newer feature
+export const sayHola = (name: string) => `Hola ${name}`;
