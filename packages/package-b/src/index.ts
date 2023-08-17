d894ea80df506c959148bb8db2162c9af0428597
Chris Alexander <chris@synaptechgenesis.com>
2023-08-17 13:05:21 -0400

    feat: add say hola to everyone

--- packages/package-b/src/index.ts
+++ packages/package-b/src/index.ts
@@ -1,5 +1,9 @@
-import { sayHello } from 'package-a';
+import { sayHello, sayHola } from 'package-a';
 
 export function sayHelloToEveryone(people: string[]): string[] {
   return people.map((person) => sayHello(person));
+}
+
+export function sayHolaToEveryone(people: string[]): string[] {
+  return people.map((person) => sayHola(person));
 }
